041491ae38a61b44924a8d3348223291beecfaa6
William <wwdouglas@hotmail.co.uk>
2023-06-14 18:26:44 +1200

    test number 2

--- src/cmp/ground.js
+++ src/cmp/ground.js
@@ -3,14 +3,23 @@ import * as THREE from "three";
 export default function createGround() {
   // Load the texture
   const textureLoader = new THREE.TextureLoader();
-  const groundTexture = textureLoader.load("/watertexture.jpg");
+  const groundTexture = textureLoader.load("/grass1.jpg");
+
+  // Load the displacement map
+  const displacementTexture = textureLoader.load("/grassdisc.jpg");
 
   groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
   groundTexture.repeat.set(25, 25);
 
   // Create the ground
-  const groundGeometry = new THREE.PlaneGeometry(500, 500);
-  const groundMaterial = new THREE.MeshBasicMaterial({ map: groundTexture });
+  const groundGeometry = new THREE.PlaneGeometry(500, 500, 64, 64); // increased the segments for better displacement
+  const groundMaterial = new THREE.MeshPhongMaterial({
+    // changed to MeshPhongMaterial for better lighting
+    map: groundTexture,
+    displacementMap: displacementTexture, // set displacement map
+    displacementScale: 10, // set displacement scale
+  });
+
   const ground = new THREE.Mesh(groundGeometry, groundMaterial);
 
   // Rotate the ground plane to be horizontal
