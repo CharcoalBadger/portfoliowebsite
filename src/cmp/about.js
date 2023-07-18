b87fcdbb448258d5c31e8ac9c0c2aedece76dbfb
William <wwdouglas@hotmail.co.uk>
2023-07-19 00:33:08 +1200

    gsap 7.20

--- src/cmp/about.js
+++ src/cmp/about.js
@@ -22,7 +22,7 @@ export default function About() {
     ScrollTrigger.create({
       trigger: ".whatiuse-section",
       start: "top top", // Change this according to where your first animation ends
-      end: "1950%",
+      end: "1940%",
       pin: ".whatiuse-section",
       pinSpacing: false,
       markers: true,
@@ -43,7 +43,7 @@ export default function About() {
     ScrollTrigger.create({
       trigger: ".versioncontrol-section",
       start: "top top", // Change this according to where your first animation ends
-      end: "1450%",
+      end: "1410%",
       pin: ".versioncontrol-section",
       pinSpacing: false,
       markers: true,
