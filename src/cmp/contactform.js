923305832faf7e3db74b1cdfdcc5bac87e974bc1
William <wwdouglas@hotmail.co.uk>
2023-07-25 22:01:19 +1200

    submit 1.4

--- src/cmp/contactform.js
+++ src/cmp/contactform.js
@@ -126,7 +126,7 @@ export default function Contactform() {
             </a>
           </div>
         </div>
-        <form name="contact" method="POST" data-netlify="true" action="/thanks">
+        {/* <form name="contact" method="POST" data-netlify="true">
           <input type="hidden" name="form-name" value="contact" />
           <p>
             <input
@@ -150,6 +150,37 @@ export default function Contactform() {
               placeholder="Your Message*"
               aria-label="Your Message"
             ></textarea>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form> */}
+        <form name="contact" method="POST" data-netlify="true">
+          <input type="hidden" name="form-name" value="Contact" />
+
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Role:{" "}
+              <select name="role[]" multiple>
+                <option value="leader">Leader</option>
+                <option value="follower">Follower</option>
+              </select>
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message"></textarea>
+            </label>
           </p>
           <p>
             <button type="submit">Send</button>
